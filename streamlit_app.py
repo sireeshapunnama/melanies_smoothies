d753141d4ee89e6259501443771394a7f6cb1e02
sireeshapunnama <sireesha.punnamaraju@accenture.com>
2025-01-28 09:37:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,7 @@ if ingredients_list:
     #new section to display smoothiefruit nutrition information
     
     import requests
-smoothiefroot_response =
-requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+    smoothiefroot_response =requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response)
 
 
